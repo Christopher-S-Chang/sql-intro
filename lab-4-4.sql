e49f62dcbbd01d20e7697fcf021017c0723ee25e
Christopher Chang <christopher.chang1@kellogg.northwestern.edu>
2025-01-21 05:26:28 +0000

    practice

--- lab-4-4.sql
+++ lab-4-4.sql
@@ -38,3 +38,10 @@
 -- +-------------------------------+------------+-------------+----------------------+
 
 
+select t.name, p.first_name as 'First', p.last_name as "Last", max(s.home_runs)
+FROM stats s  
+    INNER JOIN players p ON s.player_id = p.id
+    INNER JOIN teams t ON t.id = s.team_id
+WHERE t.year = 2019
+GROUP BY t.name
+;
